a0b01e244bbdd19698419cbd9a318e646f761712
Flip437 <philippediollot@hotmail.fr>
2020-06-10 14:14:46 +0200

    everything ok on landing page exept scroll me script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,13 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+
 //= require jquery
+//= require jquery_ujs
 //= require popper
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
+//= require bootstrap
+//= require in-view.min.js
+//= require highlight.js
 //= require theme
 //= require_tree .
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,11 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
+
+# Rails.application.config.assets.precompile += %w( @fortawesome/fontawesome-free/css/all.min.css )
+
+
+# STYLESHEETS PATH
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images", "backgrounds")
 
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images", "brand")
@@ -29,3 +34,18 @@ Rails.application.config.assets.paths << Rails.root.join("app", "assets", "image
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images", "spotlight")
 
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images", "svg")
+
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "vendor", "@fortawesome", "fontawesome-free", "css")
+
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets", "@fortawesome", "fontawesome-free", "css")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets", "highlight.js", "styles")
+# STYLESHEETS PATH
+
+
+# JAVASCRIPTS PATH
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets", "bootstrap", "dist", "js")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets", "in-view", "dist")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets", "jquery", "dist")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets", "highlight.js", "lib")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets", "clipboard", "dist")
+# JAVASCRIPTS PATH
