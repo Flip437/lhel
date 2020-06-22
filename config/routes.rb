943c359ee56f01643aafc55f6a9d7612f5d6dac6
Flip437 <philippediollot@hotmail.fr>
2020-06-22 13:25:10 +0200

    adding blog index

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'contrats/surcomplementaire'
   get 'contrats/devis'
   get 'contrats/complementaire'
+  get 'blog/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
