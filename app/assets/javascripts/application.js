346bc0f7060779d1f2b9bcd961c4866a6cd93f7a
Flip437 <philippediollot@hotmail.fr>
2020-06-23 15:55:09 +0200

    adding sub menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,3 +33,22 @@ $('header').on("mouseout",function(){
     $('header').removeClass("header-visible");
     $('header').addClass("header-transparent");
 });
+
+
+
+
+$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+    if (!$(this).next().hasClass('show')) {
+      $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    }
+    var $subMenu = $(this).next(".dropdown-menu");
+    $subMenu.toggleClass('show');
+  
+  
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+      $('.dropdown-submenu .show').removeClass("show");
+    });
+  
+  
+    return false;
+  });
