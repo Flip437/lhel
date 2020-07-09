c0ea7f0d8e5d2dab61ac9e9ab7ca996aab99c239
Flip437 <philippediollot@hotmail.fr>
2020-07-09 11:34:52 +0200

    cleaning view, adding partials for banner on static pages

--- app/controllers/contrats_controller.rb
+++ app/controllers/contrats_controller.rb
@@ -1,39 +1,51 @@
 class ContratsController < ApplicationController
 
   def mutuelle
+    @title = "Mutuelle Santé"
   end
 
   def senior
+    @title = "Mutuelle Sénior"
   end
 
   def familiale
+    @title = "Mutuelle Familiale"
   end
 
   def etudiants
+    @title = "Mutuelle Étudiante"
   end
 
   def medecinedouce
+    @title = "Médecine Douce"
   end
 
   def dentaire
+    @title = "Mutuelle Dentaire"
   end
 
   def optique
+    @title = "Mutuelle Optique"
   end
 
   def resiliation
+    @title = "Résiliation mutuelle santé"
   end
 
   def surcomplementaire
+    @title = "Surcomplémentaire Santé"
   end
 
   def devis
+    @title = "Devis"
   end
 
   def complementaire
+    @title = "Complémentaire Santé"
   end
 
   def formulaire
+    @title = "Formulaire de demande"
   end
   
 end
