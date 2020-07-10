c39a2440f455dc7616f3353b8270aa3eca03db0b
Flip437 <philippediollot@hotmail.fr>
2020-07-10 11:20:49 +0200

    adding statu selection page

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get 'blog/index'
   get 'blog/article'
   get 'contrats/formulaire'
+  get 'contrats/selection'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
