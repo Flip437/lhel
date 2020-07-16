cb4014c70e53addf12460df906c28bbb0db3e4ca
Flip437 <philippediollot@hotmail.fr>
2020-07-16 13:29:55 +0200

    moving back to env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,22 +101,22 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    # address:              'smtp.gmail.com',
-    # port:                 587,
-    # domain:               'example.com',
-    # user_name:            '<username>',
-    # password:             '<password>',
-    # authentication:       'plain',
-    # enable_starttls_auto: true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   # address:              'smtp.gmail.com',
+  #   # port:                 587,
+  #   # domain:               'example.com',
+  #   # user_name:            '<username>',
+  #   # password:             '<password>',
+  #   # authentication:       'plain',
+  #   # enable_starttls_auto: true
 
-    address:               'smtp.gmail.com',
-    port:                  587,
-    domain:                'gmail.com',
-    user_name:             ENV['GMAIL_LOGIN'],
-    password:              ENV['GMAIL_PASSWORD'],
-    authentication:        'plain',
-    enable_starttls_auto:  true}
+  #   address:               'smtp.gmail.com',
+  #   port:                  587,
+  #   domain:                'gmail.com',
+  #   user_name:             ENV['GMAIL_LOGIN'],
+  #   password:              ENV['GMAIL_PASSWORD'],
+  #   authentication:        'plain',
+  #   enable_starttls_auto:  true}
 
 end
