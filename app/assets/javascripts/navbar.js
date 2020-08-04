6077758c129f575c45704b7a5d2294542d271007
Flip437 <philippediollot@hotmail.fr>
2020-08-04 15:42:45 +0200

    working on event listener clik navbar

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -8,22 +8,9 @@ var NavbarSticky = (function() {
 		// if we've scrolled more than the navigation, change its position to fixed to stick to top,
 		// otherwise change it back to relative
 		if (scrollTop > (navOffsetTop + 200)) {
-			//$("h2").removeClass('flip-slogant-top');
-			//$("h2").addClass('flip-slogant-sticky');
-			// $(".nav-link").removeClass("top-nav-link");
-			// $(".nav-link").addClass("sticky-nav-link");
 			$(".navbar-nav").removeClass('border-top border-bottom border-white');
-			// $('#logo_space').show();
-			// $('#logo_white').hide();
-			
 		} else {
-			//$("h2").removeClass('flip-slogant-sticky');
-			//$("h2").addClass('flip-slogant-top');
-			// $(".nav-link").removeClass("sticky-nav-link");
-			// $(".nav-link").addClass("top-nav-link");
 			$(".navbar-nav").addClass('border-top border-bottom border-white');
-			// $('#logo_space').hide();
-			// $('#logo_white').show();
 		}
 	}
 	// Events
@@ -40,25 +27,12 @@ var NavbarSticky = (function() {
 	}
 })();
 
-
-
-
 $('header').on("mouseover",function(){ 
     $('header').removeClass("header-transparent");
 	$('header').addClass("header-visible");
-	// $("h2").removeClass('flip-slogant-top');
-	// $("h2").addClass('flip-slogant-sticky');
-	// $(".navbar-nav").removeClass('border-top border-bottom border-white');
-	// $('#logo_space').show();
-	// $('#logo_white').hide();
 });
 
 $('header').on("mouseout",function(){ 
     $('header').removeClass("header-visible");
 	$('header').addClass("header-transparent");
-	// $("h2").removeClass('flip-slogant-sticky');
-	// $("h2").addClass('flip-slogant-top');
-	// $(".navbar-nav").addClass('border-top border-bottom border-white');
-	// $('#logo_space').hide();
-	// $('#logo_white').show();
 });
--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,20 +3,25 @@ $(document).on('click', '.dropdown-menu', function (e) {
     e.stopPropagation();
   });
   
-  // make it as accordion for smaller screens
-  if ($(window).width() < 992) {
-    $('.dropdown-menu a').click(function(e){
+// make it as accordion for smaller screens
+if ($(window).width() < 992) {
+  $('.stop').click(function(e){
 
-      e.preventDefault();
+    e.preventDefault();
+    if($(this).next('.dropdown-item').length){    
+      $(this).next('.dropdown-item').toggle();
+    }
 
-      if($(this).next('.submenu').length){
-        $(this).next('.submenu').toggle();
-      }
+    
 
-      // to hide previously dropdowned submenus when moving beetwen dropdown button in navbar
-      $('.dropdown').on('hide.bs.dropdown', function () {
-      $(this).find('.submenu').hide();
-    })
+    if($(this).next('.submenu').length){    
+      $(this).next('.submenu').toggle();
+    }
 
-    });
-  }
+    // to hide previously dropdowned submenus when moving beetwen dropdown button in navbar
+    $('.dropdown').on('hide.bs.dropdown', function () {
+    $(this).find('.submenu').hide();
+  })
+
+  });
+}
